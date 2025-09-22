5b7a936d02b004ceea451ac47f78ea8247abaea4
opilane <opilane@HP-CZC2349HT4>
2025-09-22 11:38:41 +0300

    .

--- Video.tund_ja_Madu/HorizontalLine.cs
+++ Video.tund_ja_Madu/HorizontalLine.cs
@@ -6,10 +6,8 @@ using System.Threading.Tasks;
 
 namespace Madu
 {
-    class HorizontalLine
+    class HorizontalLine : Figure
     {
-        List<Point> pList;
-        
         public HorizontalLine(int xLeft, int xReiht, int y, char sym)
         {
             pList = new List<Point>();
@@ -19,13 +17,6 @@ namespace Madu
                 pList.Add(p);
             }
 
-        }
-        public void Drow()
-        {
-            foreach (Point p in pList)
-            {
-                p.Draw();
-            }
         }
     }
 }
--- Video.tund_ja_Madu/Madu_Start.cs
+++ Video.tund_ja_Madu/Madu_Start.cs
@@ -11,7 +11,13 @@ namespace Madu
     {
         public static void Main(string[] args)
         {
-            Console.SetBufferSize(80, 25);
+            Console.WriteLine($"Current buffer size: {Console.BufferWidth} x {Console.BufferHeight}");
+
+            // Устанавливаем новый размер буфера
+            //Console.SetBufferSize(80, 25);  // Устанавливаем размер 80x25
+
+            //// Проверяем, установился ли новый размер
+            //Console.WriteLine($"New buffer size: {Console.BufferWidth} x {Console.BufferHeight}");
 
             HorizontalLine upLine = new HorizontalLine(0,78,0,'+');
             HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
@@ -23,7 +29,39 @@ namespace Madu
             rightLine.Drow();
 
             Point p = new Point(4, 5, '*');
-            p.Draw();
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
+
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
+            while (true)
+            {
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
+
+
+
+            Console.ReadLine();
         }
     }
 }
