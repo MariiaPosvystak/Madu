fb8c76e9b77bc56f8d55c3a65b60e54c7e03d7de
opilane <opilane@HP-CZC2349HT4>
2025-09-22 10:25:40 +0300

    Add project files.

---
+++ Video.tund_ja_Madu/HorizontalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Madu
+{
+    class HorizontalLine
+    {
+        List<Point> pList;
+        
+        public HorizontalLine(int xLeft, int xReiht, int y, char sym)
+        {
+            pList = new List<Point>();
+            for(int x = xLeft; x <= xReiht; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+
+        }
+        public void Drow()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
---
+++ Video.tund_ja_Madu/Madu_Start.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Madu
+{
+    internal class Madu_Start
+    {
+        public static void Main(string[] args)
+        {
+            Console.SetBufferSize(80, 25);
+
+            HorizontalLine upLine = new HorizontalLine(0,78,0,'+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
+
+            Point p = new Point(4, 5, '*');
+            p.Draw();
+        }
+    }
+}
